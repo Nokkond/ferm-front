ad80fbd3e62e3ecb77ad11cbf44cff8c1c7cb6b9
Nokkond <radygin.n@yandex.ru>
2022-11-27 18:56:42 +0300

    test-fix

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ import Login from "./components/popups/Login.vue";
     </div>
   </header>
 <!--    <Auth />-->
-    <Login />
+<!--    <Login />-->
 <!--    <CreateCard />-->
   <RouterView />
 </template>
