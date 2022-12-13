497ca15647bc69cc4e1f043bfcae0b41ccd5b016
Nokkond <radygin.n@yandex.ru>
2022-12-13 07:58:16 +0300

    final commit 2

---
+++ src/views/CreateView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import CreateCard from "../components/popups/CreateCard.vue"
+</script>
+
+<template>
+    <main  style="margin-top: -150px !important;">
+        <CreateCard/>
+    </main>
+</template>
+
